81bc27817b568cc452ce906740de7b8166f12179
VedashreeDevrukhkar-17 <vedashreedevrukhkar8@gmail.com>
2021-12-07 21:48:30 +0530

    modify main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,19 @@ class intro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage("")),
+              Text("Vedashree Sudhir Devrukhkar"),
+              Text("vedashreedevrukhkar8@gmail.com"),
+              Text("Dancing"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
